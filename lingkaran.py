c0a925ecbf7d6c0d1ef8b734fc7e9b033ad279ba
= <=>
2024-10-08 09:02:43 +0700

    create aplikasi lingkaran

--- lingkaran.py
+++ lingkaran.py
@@ -1,5 +1,11 @@
-print("=================")
+print("="*40)
 print("PROGRAM LINGKARAN")
-print("=================")
+print("="*40)
 
 jarijari = int(input("Masukkan jarijari :"))
+
+luas = jarijari/2
+keliling = 2*3.14*jarijari
+
+print('luas',luas, 'cm2')
+print('keliling',keliling, 'cm')
